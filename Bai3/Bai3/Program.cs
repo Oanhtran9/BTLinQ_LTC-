22e69cdf2c566818a2f7230835302815a39a26d5
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-03-18 23:50:39 +0700

    Bài 3. Sắp xếp tăng dần

---
+++ Bai3/Bai3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Bai3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tran Thi Oanh - 23115053122330");
+            List<int>danhsach=new List<int> {6,3,9,1,7,-1,5,2,-5,8,5};
+            
+            Console.WriteLine("\nDanh sach: ");
+            foreach(int i in danhsach)
+            {
+                Console.Write(i+" ");
+            }
+            var tangdan=danhsach.OrderBy(so=>so);
+            Console.WriteLine("\n\nDanh sach so tang dan: ");
+            foreach(int i in tangdan)
+            {
+                Console.Write(i+" ");
+            }
+        }
+    }
+}
