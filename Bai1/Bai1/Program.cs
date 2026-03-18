97ab3af95124ae45788eedf3fa2ea4db633448c5
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-03-18 22:43:51 +0700

    Bài 1. Lọc số chẵn

---
+++ Bai1/Bai1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tran Thi Oanh - 23115053122330");
+            List <int> numbers = new List<int> { 1,2,3,4,5,6,7,8};
+            var sochan = numbers.Where(so=>so%2==0);
+            Console.WriteLine("\nSo chan trong danh sach: ");
+            foreach(var so in sochan )
+            {
+                Console.WriteLine(so);
+            }
+        }
+    }
+}
