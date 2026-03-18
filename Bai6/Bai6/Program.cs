901b96aadc3557ea5d2cb9e9346498a840be61b4
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-03-19 01:02:28 +0700

    Bài 6. Đếm số chẵn

---
+++ Bai6/Bai6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Bai6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tran Thi Oanh - 23115053122330");
+            List <int> danhsach = new List<int>();
+            Random rd= new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                danhsach.Add(rd.Next(1, 100));
+            }
+            Console.WriteLine("Danh sach ngau nhien: " );
+            foreach (var so in danhsach)
+            {
+                Console.Write(so + " ");
+            }
+            var sochan = danhsach.Where(so => so % 2 == 0);
+            Console.WriteLine("\nTong so chan: " + sochan.Count());
+        }
+    }
+}
