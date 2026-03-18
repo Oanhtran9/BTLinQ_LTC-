ba56cc63624a7ab06caa177bf092d5d3942b0866
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-03-19 00:36:18 +0700

    Bài 4. Sắp xếp giảm dần

---
+++ Bai4/Bai4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Bai4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tran Thi Oanh - 23115053122330");
+            List<int> danhsach = new List<int>();
+            Random rd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                danhsach.Add(rd.Next(1, 100));
+            }
+            Console.WriteLine("Danh sach so nguyen:");
+            foreach (int so in danhsach)
+            {
+                Console.WriteLine(so);
+            }
+            var giamdan = danhsach.OrderByDescending(so => so);
+            Console.WriteLine("Danh sach giam danh: ");
+            foreach (int so in giamdan)
+            {
+                Console.WriteLine(so);
+            }
+        }
+    }
+}
