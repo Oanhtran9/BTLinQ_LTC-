aa8c26c1f7c707e9e4d7626eda0754ad35552e61
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-03-18 22:55:18 +0700

    Bài 2. Lọc số lớn hơn 5

---
+++ Bai2/Bai2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tran Thi Oanh - 23115053122330");
+            List<int> numbers=new List<int> { 1,2,3,4,5,6,7,8,9,10};
+            var solonhon5 = numbers.Where(so => so > 5);
+            Console.WriteLine("\ndanh sach so lon hon 5:");
+            foreach(var so in solonhon5)
+            {
+                Console.WriteLine(so);
+            }
+        }
+    }
+}
