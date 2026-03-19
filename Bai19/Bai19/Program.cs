4d827cfde517571a9c30243aa45ac195781fe353
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-03-19 23:45:21 +0700

    Bài 19. Tìm số xuất hiện nhiều nhất

--- Bai19/Bai19/Program.cs
+++ Bai19/Bai19/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Bai19
 {
@@ -6,7 +8,26 @@ namespace Bai19
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tran Thi Oanh - 23115053122330-225LTC#02");
+            List<int> danhsach = new List<int>();
+            Random ds = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                danhsach.Add(ds.Next(1, 10));
+            }
+            Console.WriteLine("\nDanh sach:");
+            foreach (var so in danhsach)
+            {
+                Console.Write(so + " ");
+            }
+            var max = danhsach
+               .GroupBy(so => so)
+               .Select(n => new { so = n.Key, Dem = n.Count() })
+               .OrderByDescending(so => so.Dem)
+               .First();
+            Console.WriteLine("\n\nSo xuat hien nhieu nhat: " + max.so);
+            Console.WriteLine("So lan: " + max.Dem);
         }
     }
 }
--- Bai19/Bai19/obj/Debug/net5.0/Bai19.AssemblyInfo.cs
+++ Bai19/Bai19/obj/Debug/net5.0/Bai19.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai19")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b9eea74e77736aa9e14f88e11750a62a63efab5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d72959adde07a5e787972d9a187a32dddaaa975")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai19")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai19")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
