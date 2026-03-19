4498bd7ae03a4af95485259729c631c47039c5e1
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-03-20 00:05:38 +0700

    update Bài 1. Lọc số chẵn

--- Bai1/Bai1/Program.cs
+++ Bai1/Bai1/Program.cs
@@ -8,10 +8,15 @@ namespace Bai1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tran Thi Oanh - 23115053122330");
+            Console.WriteLine("Tran Thi Oanh - 23115053122330-225LTC#02");
             List <int> numbers = new List<int> { 1,2,3,4,5,6,7,8};
+            Console.Write("\nDanh sach so: ");
+            foreach(var so in numbers)
+            {
+                Console.Write(so+" ");
+            }
             var sochan = numbers.Where(so=>so%2==0);
-            Console.WriteLine("\nSo chan trong danh sach: ");
+            Console.WriteLine("\n\nSo chan trong danh sach: ");
             foreach(var so in sochan )
             {
                 Console.WriteLine(so);
