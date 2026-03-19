d7a9e32d491a1e12eb709218d0bbfc92cd313723
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-03-20 00:08:49 +0700

    update

--- Bai2/Bai2/Program.cs
+++ Bai2/Bai2/Program.cs
@@ -8,7 +8,7 @@ namespace Bai2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tran Thi Oanh - 23115053122330");
+            Console.WriteLine("Tran Thi Oanh - 23115053122330 - 225LTC#02");
             List<int> numbers=new List<int> { 1,2,3,4,5,6,7,8,9,10};
             var solonhon5 = numbers.Where(so => so > 5);
             Console.WriteLine("\ndanh sach so lon hon 5:");
