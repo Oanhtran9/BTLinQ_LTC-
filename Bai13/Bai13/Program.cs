109ddd183781f54a9d6176fc3e87de79c0cfbe73
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-03-19 21:34:04 +0700

    Commit Bai 13

---
+++ Bai13/Bai13/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bai13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
