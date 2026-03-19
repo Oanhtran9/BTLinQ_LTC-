1f7431666491a7f68f1b81f365ffbd199e97a904
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-03-19 22:51:19 +0700

    Commit bài 18

---
+++ Bai18/Bai18/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bai18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
