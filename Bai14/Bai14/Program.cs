9bc7e4bcf712aff7cefb01fb7a2a8eee954796ab
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-03-19 21:53:00 +0700

    Bài 14. Kiểm tra có sinh viên rớt không

--- Bai14/Bai14/Program.cs
+++ Bai14/Bai14/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace Bai14
 {
+    class students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Sorce { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tran Thi Oanh - 23115053122330");
+            List<students> sinhvien = new List<students>()
+            {
+                new students { Id = 1,Name="An", Sorce = 6.5},
+                new students { Id = 2,Name="Binh", Sorce = 7},
+                new students { Id = 3,Name="Cuong", Sorce = 4.5},
+                new students { Id = 4,Name="Dung", Sorce = 6},
+                new students { Id = 5,Name="Hai", Sorce = 8.5},
+                new students { Id = 6,Name="Hoa", Sorce = 3}
+            };
+            var kiemtra = sinhvien.Where(sv => sv.Sorce < 5);
+            if (kiemtra.Count() == 0)
+            {
+                Console.WriteLine("\nKhong co sinh vien nao bi rot");
+            }
+            else
+            {
+                Console.WriteLine("\nDanh sach sinh vien bi rot:");
+                foreach (var sv in kiemtra)
+                {
+                    Console.WriteLine($"Id: {sv.Id}, Name: {sv.Name}, Sorce: {sv.Sorce}");
+                }
+            }
         }
     }
 }
--- Bai14/Bai14/obj/Debug/net5.0/Bai14.AssemblyInfo.cs
+++ Bai14/Bai14/obj/Debug/net5.0/Bai14.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai14")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5885e71a56777349eee1afdbba9f3d343122e70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3992fee81d2b0f3a8a04353b8e678391c3cd7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai14")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
