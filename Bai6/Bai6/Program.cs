72045d231af7fe125a9d6318788c63d5293d592a
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-03-20 00:18:15 +0700

    update

--- Bai6/Bai6/Program.cs
+++ Bai6/Bai6/Program.cs
@@ -7,7 +7,7 @@ namespace Bai6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tran Thi Oanh - 23115053122330");
+            Console.WriteLine("Tran Thi Oanh - 23115053122330 - 225LTC#02 ");
             List <int> danhsach = new List<int>();
             Random rd= new Random();
             for (int i = 0; i < 10; i++)
--- Bai6/Bai6/obj/Debug/net5.0/Bai6.AssemblyInfo.cs
+++ Bai6/Bai6/obj/Debug/net5.0/Bai6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5885e71a56777349eee1afdbba9f3d343122e70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+901b96aadc3557ea5d2cb9e9346498a840be61b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
