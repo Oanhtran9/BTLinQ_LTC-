46501325f9d8f7a9ca3227f69b9fcdc81f6d1869
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-03-19 13:29:26 +0700

    update Bài 8 Lọc sinh viên điểm >= 8

--- Bai8/Bai8/Program.cs
+++ Bai8/Bai8/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace Bai8
 {
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tran Thi Oanh - 23115053122330");
+            List<Student> students = new List<Student>()
+            {   new Student { Id = 1, Name = "An", Score = 8 },
+                new Student { Id = 2, Name = "Binh", Score = 6 },
+                new Student { Id = 3, Name = "Chi", Score = 9 },
+                new Student { Id = 4, Name = "Dung", Score = 7 }
+            };
+            var svgioi = students.FindAll(sv => sv.Score >= 8);
+            Console.WriteLine("Danh sach sinh vien co diem >= 8:");
+            foreach (var sv in svgioi)
+            {
+                Console.WriteLine($"Id: {sv.Id}, Name: {sv.Name}, Score: {sv.Score}");
+            }
         }
     }
 }
--- Bai8/Bai8/obj/Debug/net5.0/Bai8.AssemblyInfo.cs
+++ Bai8/Bai8/obj/Debug/net5.0/Bai8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5885e71a56777349eee1afdbba9f3d343122e70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+746b5fbd7e530d5b143c4542b52d1be1c7ad7d69")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
