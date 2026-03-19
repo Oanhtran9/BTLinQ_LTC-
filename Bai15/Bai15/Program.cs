fd763b1086920d43c50abe3e3ea05cd3be89a21b
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-03-19 22:06:37 +0700

    Commit Bài 15

---
+++ Bai15/Bai15/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bai15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
