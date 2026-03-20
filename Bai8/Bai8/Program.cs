88315eccf838bd545afae6741db357f4293335e0
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-03-20 18:22:57 +0700

    update Bài 8

--- Bai8/Bai8/Program.cs
+++ Bai8/Bai8/Program.cs
@@ -14,7 +14,7 @@ namespace Bai8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tran Thi Oanh - 23115053122330");
+            Console.WriteLine("Tran Thi Oanh - 23115053122330 - 225LTC#02 ");
             List<Student> students = new List<Student>()
             {   new Student { Id = 1, Name = "An", Score = 8 },
                 new Student { Id = 2, Name = "Binh", Score = 6 },
@@ -22,7 +22,7 @@ namespace Bai8
                 new Student { Id = 4, Name = "Dung", Score = 7 }
             };
             var svgioi = students.FindAll(sv => sv.Score >= 8);
-            Console.WriteLine("Danh sach sinh vien co diem >= 8:");
+            Console.WriteLine("\nDanh sach sinh vien co diem >= 8:");
             foreach (var sv in svgioi)
             {
                 Console.WriteLine($"Id: {sv.Id}, Name: {sv.Name}, Score: {sv.Score}");
--- Bai8/Bai8/obj/Debug/net5.0/Bai8.AssemblyInfo.cs
+++ Bai8/Bai8/obj/Debug/net5.0/Bai8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+746b5fbd7e530d5b143c4542b52d1be1c7ad7d69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46501325f9d8f7a9ca3227f69b9fcdc81f6d1869")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
