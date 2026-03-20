0f5a28abbb9556b63b13d4eced131df3cbaf88b0
Oanhtran09 <tranthioanhh11.6@gmail.com>
2026-03-20 18:45:14 +0700

    update Bài 14

--- Bai14/Bai14/obj/Debug/net5.0/Bai14.AssemblyInfo.cs
+++ Bai14/Bai14/obj/Debug/net5.0/Bai14.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai14")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3992fee81d2b0f3a8a04353b8e678391c3cd7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bc7e4bcf712aff7cefb01fb7a2a8eee954796ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai14")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
